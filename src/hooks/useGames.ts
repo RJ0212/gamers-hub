a17edf0f1af294d964b203ebd5f4cb0f2fd93583
RJ C. Pabadora <pabadorarj@gmail.com>
2023-05-14 12:36:36 +0800

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,7 +23,8 @@ const useGames = (gameQuery: GameQuery) =>
   useData<Game>('/games', {
     params: {
       genres: gameQuery.genre?.id, 
-      platforms: gameQuery.platform
+      platforms: gameQuery.platform?.id,
+      ordering: gameQuery.sortOrder
     }}, 
     [gameQuery]);
 
